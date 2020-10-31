202ea33e283e5bdf952a0fe01409a218aa6d0622
Jeremy Andes <jeremyandes25@gmail.com>
2020-10-30 21:43:19 -0300

    Carga del archivo txt y manejo de listas con objetos funcionando.

--- prueba.py
+++ prueba.py
@@ -1,18 +1,37 @@
 #clase "Proceso"
 #campo = "valor posible" // pID = >1 && <9999 // tda = >0 && <99999 segundos // pdp = >1 && <15 // tdp = >0 && <9999 segundos
 class Proceso:
-    def __init__(self, campo, pID, tda, pdp, tdp):
-        self.campo = campo
-        self.pID = pID
-        self.tda = tda
-        self.pdp = pdp
-        self.tdp = tdp
+    def __init__(self):
+        self.campo = ""
+        self.pID = -1
+        self.tda = -1
+        self.pdp = -1
+        self.tdp = -1
     def muestra(self):
         return '{}, {}, {}, {}, {}'.format(self.campo,self.pID,self.tda,self.pdp,self.tdp)
-    def muestra_fstring(self):
-        return '% s, % s, % s, % s, % s'%(self.campo,self.pID,self.tda,self.pdp,self.tdp)
 
-proceso1 = Proceso('Campo1', 2500, 10, 12, 1)
+class ListaProcesos(list):
 
-print(proceso1.muestra() + "\n------------------------------")
-print(proceso1.muestra_fstring())
+    f = open ("procesos.txt", "r")
+
+    def __init__(self):
+        for linea in self.f:
+            self.append(self.setProceso(linea))
+
+    def setProceso(self, linea):
+        datos = linea.split(".")
+        procesoNuevo = Proceso()
+        procesoNuevo.pID = datos[0]
+        procesoNuevo.tda = datos[1]
+        procesoNuevo.pdp = datos[2]
+        procesoNuevo.tdp = datos[3]
+        return procesoNuevo
+
+    def getProcesos(self):
+        for obj in self:
+            # print(obj.pID, obj.tda, obj.pdp, obj.tdp)
+            print("pID: {}\ntda: {}\npdp: {}\ntdp: {}\n-----------------------------------"
+            .format(obj.pID, obj.tda, obj.pdp, obj.tdp))
+
+Procesos = ListaProcesos()
+Procesos.getProcesos()
