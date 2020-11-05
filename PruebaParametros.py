366ce3394c0cd445f4e426265d11ed3840adc0d5
LuzGodoy <godoydq@gmail.com>
2020-11-05 15:49:53 -0300

    Avanzando con parametros, funcionan OK.

--- PruebaParametros.py
+++ PruebaParametros.py
@@ -1,41 +1,43 @@
 #Parametros del sistema
-from optparse import OpcionParser
-'''Algoritmo de scheduling de procesador (FCFS, SJF sin desalojo,
- Prioridades sin desalojo, Round Robin).
-Valor del Quantum (si el algoritmo de scheduling de procesador 
-lo requiere). Se mide en segundos. 
-Cantidad de threads (opcionalmente se podrán ejecutar utilizando 
-múltiples thread), esto sólo está disponible para FCFS y SJF y 
-Prioridades.
-parser.add_option("-f", "--file", dest="filename",
-                  help="write report to FILE", metavar="FILE")
-parser.add_option("-q", "--quiet",
-                  action="store_false", dest="verbose", default=True,
-                  help="don't print status messages to stdout")
-action="store", type="string"
-(options, args) = parser.parse_args()'''
+from optparse import OptionParser
 
-parser=OptionParser()
-parser.add_option("-a","--algoritmo", action="store", type="string", dest="algoritmo",
-            choices=["FIFO","SJF","PR","RR"],
-            help="Escriba el algoritmo de scheduling de procesador"
-parser.add_option("-q", "--quantum", action="store", type="int", dest="quantum", default=2,
-            help="Especifique la duracion del quantum en segundos")
-parser.add_option("-t","--thread", action="store_true", dest="hilo", default="false",
-            help="indique la cantidad de hilos")
-(options, args) = parser.parse_args()
-
-if options.algoritmo==None
-    print("Debe indicar el algoritmo a utilizar")
-else:
-    print("El algorimo elegido fue")
-    switch_algoritmo(options.algoritmo)
-
-def switch_algortimo(argument):
+#Funcion que devuelve el nombre del algoritmo segun la sigla
+def switch_algoritmo(argument):
     switcher = {
         "FIFO": "First in First out",
-        "SJF": "Primero el mas corto",
-        "PR": "Prioridades",
-        "RR": "Round Robin",
+        "SJF" : "Primero el mas corto",
+        "PR"  : "Prioridades",
+        "RR"  : "Round Robin"
     }
-    print switcher.get(argument, "entrada invalida")
+    return switcher.get(argument, "entrada invalida")
+
+#Definicion de las opciones
+parser=OptionParser()
+parser.add_option("-a","--algoritmo", choices=["FIFO","SJF","PR","RR"],
+            help="Escriba el algoritmo de scheduling de procesador")
+parser.add_option("-q", "--quantum", action="store", type="int", dest="quantum", default=2,
+            help="Especifique la duracion del quantum en segundos")
+parser.add_option("-t","--thread", action="store", type="int", dest="hilo", 
+            help="indique la cantidad de hilos")
+
+#Almacena los parametros del sistema en los atributos de options
+(options, args) = parser.parse_args()
+
+al=options.algoritmo
+if al==None :
+    print("Debe indicar el algoritmo a utilizar")
+else:
+    print("El algorimo elegido fue ", switch_algoritmo(al))
+
+if al == 'RR':
+    print("El quantum es de ", options.quantum)
+else:
+    pass
+
+if al== "FIFO" or al=="SJF" or al=="PR" :
+    if options.hilo==None :
+        print("No se especifico la cantidad de hilos a utilizar")
+    else:
+        print("Se utilizaran {} hilos".format( str(options.hilo) ))
+
+
